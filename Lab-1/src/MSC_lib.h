177024b63ae2926fcf475cc53ca0bbb1d23c30ed
Doshinsky <akter1997@gmail.com>
2017-02-23 14:21:02 +0700

    Added needed funcs

--- Lab-1/src/MSC_lib.h
+++ Lab-1/src/MSC_lib.h
@@ -1,6 +1,15 @@
 #ifndef MSCLIB_H
 #define MSCLIB_H
 
-/* headers */
+int sc_memoryInit(); /* Инициализирует оперативную память SC, задавая всем её ячейкам нулевые значения. */
+int sc_memorySet(int address, int value); /* Задает значение указанной ячейки памяти как value. */
+int sc_memoryGet(int address, int *value); /* Возвращает значение указанной ячейки памяти в value. */
+int sc_memorySave(char *filename); /* Сохраняет содержимое памяти в файл в бинарном виде. */
+int sc_memoryLoad(char *filename); /* Загружает из указанного файла содержимое оперативной памяти. */
+int sc_regInit(void); /* Инициализирует регистр флагов нулевым значением. */
+int sc_regSet(int register, int value); /* Устанавливает значение указанного регистра флагов. */
+int sc_regGet(int register, int *value); /* Возвращает значение указанного флага. */
+int sc_commandEncode(int command, int operand, int *value); /* Кодирует команду с указанным номером и операндом и помещает результат в value. */
+int sc_commandDecode (int value, int *command, int *operand); /* Декодирует значение как команду SC. */
 
 #endif
