af362e2b5a283011361d08f4b14c2e11dcc9bafe
Doshinsky <akter1997@gmail.com>
2017-03-01 10:53:39 +0700

    Created blanks

---
+++ Lab-2/src/MT_lib.c
@@ -0,0 +1,31 @@
+#include "MT_lib.h"
+
+int mt_clrscr()
+{
+
+}
+
+int mt_gotoXY(int x, int y)
+{
+
+}
+
+int mt_getscreensize(int *rows, int *cols)
+{
+
+}
+
+int mt_setfgcolor(enum colors)
+{
+
+}
+
+int mt_setbgcolor(enum colors)
+{
+
+}
+
+void CONSOLE_TEST()
+{
+	
+}
---
+++ Lab-2/src/MT_main.c
@@ -0,0 +1,7 @@
+#include "MT_lib.h"
+
+int main(int argc, char *argv[])
+{
+	CONSOLE_TEST();
+	return EXIT_SUCCESS;
+}
