9fbfc5045cb952f9a0e78eda968e65bebadb38cc
Doshinsky <akter1997@gmail.com>
2017-03-01 10:56:42 +0700

    Created colors funcs

--- Lab-2/src/MT_lib.c
+++ Lab-2/src/MT_lib.c
@@ -43,12 +43,84 @@ int mt_getscreensize(int *rows, int *cols)
 
 int mt_setfgcolor(enum colors)
 {
+	int color;
+	switch(colors)
+	{
+		case black: 
+			color = 0; 
+			break;
+		case red: 
+			color = 1; 
+			break;
+		case green:
+			color = 2;
+			break;
+		case yellow:
+			color = 3;
+			break;
+		case blue:
+			color = 4;
+			break;
+		case purple: 
+			color = 5;
+			break;
+		case cyan:
+			color = 6;
+			break;
+		case white:
+			color = 7;
+			break;
+		default:
+			return -1;
+	}
 
+	char buf[8] = "\E[3", color1[2] = {0};
+	sprintf(color1, "%d", color);
+	strcat(buf, color1);
+	strcat(buf, "m");
+	write(1, buf, sizeof(buf));
+	return 0;
 }
 
 int mt_setbgcolor(enum colors)
 {
+	int color;
+	switch(colors)
+	{
+		case black: 
+			color = 0; 
+			break;
+		case red: 
+			color = 1; 
+			break;
+		case green:
+			color = 2;
+			break;
+		case yellow:
+			color = 3;
+			break;
+		case blue:
+			color = 4;
+			break;
+		case purple: 
+			color = 5;
+			break;
+		case cyan:
+			color = 6;
+			break;
+		case white:
+			color = 7;
+			break;
+		default:
+			return -1;
+	}
 
+	char buf[8] = "\E[4", color1[2] = {0};
+	sprintf(color1, "%d", color);
+	strcat(buf, color1);
+	strcat(buf, "m");
+	write(1, buf, sizeof(buf));
+	return 0;
 }
 
 void CONSOLE_TEST()
